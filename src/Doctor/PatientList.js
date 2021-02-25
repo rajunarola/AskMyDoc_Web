1b7844087b3629b377252558e07670e3f652fd31
Chidrup Desai <dchidrup@narolainfotech.com>
2021-02-25 09:50:37 +0530

    logo in doctor

--- src/Doctor/PatientList.js
+++ src/Doctor/PatientList.js
@@ -18,7 +18,7 @@ export default class PatientList extends Component
   }
   componentDidMount() {
     if (localStorage.getItem('Token') === null) {
-      props.history.push('/')
+      this.props.history.push('/doctor')
 
     }
     this.displayPatientList();
